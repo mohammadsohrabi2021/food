4397d1dcd5f97cdad6958a94fbfe44c84a8cdb13
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-22 00:59:45 +0330

    create component main meunPge and file style done

---
+++ components/templates/MenuPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from '../modules/Card'
+import styles from './MenuPage.module.css'
+function MenuPage({data}) {
+  return (
+    <div className={styles.container}>
+    <h2>Menu</h2>
+    <div className={styles.subContainer}>
+      {data.map((food) => (
+        <Card key={food.id} {...food} />
+      ))}
+    </div>
+  </div>
+  )
+}
+
+export default MenuPage
+
